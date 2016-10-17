2cabacef4ebdf8703c8f5a977ddeb75766d7407c
Jeremy Henault <jeremy@jhenault.net>
2016-10-16 23:55:08 -0700

    Revert "using an asset pipeline" The jekyll-asset plugin is not supported by gh pages. Will need to do my own pipeline.
    This reverts commit 41b00dec75f04609985e15abc7496f6146ef8591.

--- _assets/js/main.js
+++
@@ -1,8 +0,0 @@
-//= require jquery-2.1.4.min
-//= require isotope.pkgd.min
-//= require jquery.magnific-popup.min
-//= require jquery.animsition.min
-//= require wow.min
-//= require idangerous.swiper.min.js
-//= require all
-//= require contact
